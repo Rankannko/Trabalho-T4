6c14e62f45d6f2d9e60b78e51a042afea50f054d
Rankannko <azevedomasterjp27@hotmail.com>
2020-11-26 19:33:26 -0300

    Menu implementação
    Implementação da classe menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
+import com.company.classes.Menu;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Menu menu = new Menu();
+	menu.run();
     }
 }
---
+++ src/com/company/classes/Menu.java
@@ -0,0 +1,7 @@
+package com.company.classes;
+
+public class Menu {
+    public void run(){
+        System.out.println("O menu está funcionando");
+    }
+}
