134eecabcdb3e6c73c673ef4cdb40f69af06119e
Rankannko <azevedomasterjp27@hotmail.com>
2020-11-26 19:52:07 -0300

    Criação personagem raça e profissao
    Criação de recursos para o trabalho como classes ou enums

---
+++ src/com/company/classes/Personagem.java
@@ -0,0 +1,6 @@
+package com.company.classes;
+
+public class Personagem {
+    private String Nome, Profissão;
+    private int Mana, Ataque, AtaqueMagico, Defesa, DefesaMagica, Velocidade, Destreza, Experiencia, Nivel;
+}
