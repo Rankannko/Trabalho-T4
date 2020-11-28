4a2f2598cc0af0d1e39eb0077e8d91bd9a92d800
Rankannko <azevedomasterjp27@hotmail.com>
2020-11-28 11:33:32 -0300

    Criaçao de um personagem

--- src/com/company/classes/Menu.java
+++ src/com/company/classes/Menu.java
@@ -11,11 +11,30 @@ public class Menu {
         scanner = new Scanner(System.in);
     }
     public void run(){
-
+        GerarPersonagem();
+        System.out.println(personagem);
     }
     public void GerarPersonagem(){
-        System.out.println("Digite o nome do personagem:");
-        String nome = scanner.nextLine();
+        String Nome;
+        Classes classe;
+        Racas raca;
+        int Mana, Ataque, AtaqueMagico, Defesa, DefesaMagica, Velocidade, Destreza, Experiencia, Nivel;
+        System.out.println("Digite os dados:");
+        Nome = scanner.nextLine();
         System.out.println("Digite a raça do personagem:");
+        raca = Racas.valueOf(scanner.nextLine());
+        System.out.println("Digite a classe do personagem");
+        classe = Classes.valueOf(scanner.nextLine());
+        System.out.println("Digite a Mana, Ataque, Ataque Magico, Defesa, Defesa Magicos, Velocidade, Destreza, Experienca e Nivel do personagem:");
+        Mana = Integer.parseInt(scanner.nextLine());
+        Ataque = Integer.parseInt(scanner.nextLine());
+        AtaqueMagico = Integer.parseInt(scanner.nextLine());
+        Defesa = Integer.parseInt(scanner.nextLine());
+        DefesaMagica= Integer.parseInt(scanner.nextLine());
+        Velocidade= Integer.parseInt(scanner.nextLine());
+        Destreza = Integer.parseInt(scanner.nextLine());
+        Experiencia= Integer.parseInt(scanner.nextLine());
+        Nivel= Integer.parseInt(scanner.nextLine());
+        new Personagem(Nome,classe,raca,Mana, Ataque, AtaqueMagico, Defesa, DefesaMagica, Velocidade, Destreza, Experiencia, Nivel);
     }
 }
--- src/com/company/classes/Personagem.java
+++ src/com/company/classes/Personagem.java
@@ -3,9 +3,8 @@ import com.company.Enums.Classes;
 import com.company.Enums.Racas;
 
 public class Personagem {
-    public Personagem(String nome, String profissão, Classes classe, Racas raca, int mana, int ataque, int ataqueMagico, int defesa, int defesaMagica, int velocidade, int destreza, int experiencia, int nivel) {
+    public Personagem(String nome, Classes classe, Racas raca, int mana, int ataque, int ataqueMagico, int defesa, int defesaMagica, int velocidade, int destreza, int experiencia, int nivel) {
         Nome = nome;
-        Profissão = profissão;
         this.classe = classe;
         this.raca = raca;
         Mana = mana;
@@ -19,8 +18,75 @@ public class Personagem {
         Nivel = nivel;
     }
 
-    private String Nome, Profissão;
+    @Override
+    public String toString() {
+        return "Personagem{" +
+                "Nome='" + Nome + '\'' +
+                ", classe=" + classe +
+                ", raca=" + raca +
+                ", Mana=" + Mana +
+                ", Ataque=" + Ataque +
+                ", AtaqueMagico=" + AtaqueMagico +
+                ", Defesa=" + Defesa +
+                ", DefesaMagica=" + DefesaMagica +
+                ", Velocidade=" + Velocidade +
+                ", Destreza=" + Destreza +
+                ", Experiencia=" + Experiencia +
+                ", Nivel=" + Nivel +
+                '}';
+    }
+
+    private String Nome;
     private Classes classe;
     private Racas raca;
     private int Mana, Ataque, AtaqueMagico, Defesa, DefesaMagica, Velocidade, Destreza, Experiencia, Nivel;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public Classes getClasse() {
+        return classe;
+    }
+
+    public Racas getRaca() {
+        return raca;
+    }
+
+    public int getMana() {
+        return Mana;
+    }
+
+    public int getAtaque() {
+        return Ataque;
+    }
+
+    public int getAtaqueMagico() {
+        return AtaqueMagico;
+    }
+
+    public int getDefesa() {
+        return Defesa;
+    }
+
+    public int getDefesaMagica() {
+        return DefesaMagica;
+    }
+
+    public int getVelocidade() {
+        return Velocidade;
+    }
+
+    public int getDestreza() {
+        return Destreza;
+    }
+
+    public int getExperiencia() {
+        return Experiencia;
+    }
+
+    public int getNivel() {
+        return Nivel;
+    }
+
 }
